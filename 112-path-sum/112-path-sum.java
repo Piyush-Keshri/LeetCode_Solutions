8c871753a8bb886ae3e50295b2e308358d82a783
Piyush Keshri <69593831+Piyush-Keshri@users.noreply.github.com>
2022-06-03 22:42:39 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (84.11%) - LeetHub

--- 112-path-sum/112-path-sum.java
+++ 112-path-sum/112-path-sum.java
@@ -14,14 +14,11 @@
  * }
  */
 class Solution {
-    public boolean hasPathSum(TreeNode root, int targetSum) {
-        if(root == null){
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null)
             return false;
-        }
-        if(root.left == null && root.right == null){
-            return (targetSum == root.val);
-        }
-
-        return hasPathSum(root.left,targetSum-root.val) || hasPathSum(root.right,targetSum-root.val);  
+        else
+            return (root.left == null && root.right == null && root.val == sum)
+            || hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
     }
 }
