21e9cc278a89838d9b9e04aad32183c09b30e2bd
Piyush Keshri <69593831+Piyush-Keshri@users.noreply.github.com>
2022-06-04 15:56:27 +0530

    Time: 0 ms (100.00%), Space: 43.5 MB (65.08%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -1,18 +1,14 @@
-
-class Solution {
-    public boolean isValidBST(TreeNode root) {
-       return validBST(root,Long.MIN_VALUE,Long.MAX_VALUE);    
-}
-    
-    public boolean validBST(TreeNode root, long minVal,long maxVal){
+class Solution{
+    public boolean isValidBST(TreeNode root){
+        return validBST(root,Long.MIN_VALUE,Long.MAX_VALUE);
+    }
+    public boolean validBST(TreeNode root,long minVal,long maxVal){
         if(root == null){
             return true;
         }
-        
-        if(root.val>=maxVal || root.val<=minVal){
+        if(root.val<= minVal || root.val>=maxVal){
             return false;
         }
-        
-       return validBST(root.left,minVal,root.val) && validBST(root.right,root.val,maxVal);
+        return validBST(root.left,minVal,root.val) && validBST(root.right,root.val,maxVal);
     }
 }
