95dcba30abcc772c706764b1ddcfa4425295abf5
Piyush Keshri <69593831+Piyush-Keshri@users.noreply.github.com>
2022-07-02 16:19:35 +0530

    Time: 3 ms (35.06%), Space: 54.6 MB (58.63%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -3,15 +3,19 @@ class Solution {
 
         int[] res = new int[nums.length];
         int j = 0;
+        
+//         Storing the non-zero values in new array
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
                 res[j] = nums[i];
                 j++;
             }
         }
+//          populating the new array with zeros after non-zero values        
         for(int k = res.length; k<nums.length;k++){
             res[k] = 0;
         }
+//        copying the new array to given array. 
         for(int p = 0;p<nums.length;p++){
             nums[p] = res[p];
         }
