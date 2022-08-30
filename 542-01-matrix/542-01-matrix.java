16d46ee6cd321634c01d1792f656682a6798fd8b
Piyush Keshri <69593831+Piyush-Keshri@users.noreply.github.com>
2022-08-30 11:12:18 +0530

    Time: 22 ms (51.74%), Space: 75.1 MB (12.17%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -1,31 +1,27 @@
 class Node{
-    int row;
-    int col;
-    int dist;
-    
-    Node(int row,int col,int dist){
-        this.row = row;
-        this.col = col;
+    int first; int second; int dist;
+    Node(int first, int second, int dist){
+        this.first = first;
+        this.second = second;
         this.dist = dist;
     }
-    
 }
 
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
         
-        int n = mat.length;
-        int m = mat[0].length;
+    int n = mat.length;
+    int m = mat[0].length;
+    
+        int[][] vis = new int [n][m];
+        int[][] dist = new int[n][m];
         
-        int[][] vis = new int[n][m];
-        int[][] ans = new int[n][m];
-        Queue<Node> q = new LinkedList<>();
+        Queue<Node> q = new LinkedList<Node>();
         
         for(int i = 0;i<n;i++){
             for(int j=0;j<m;j++){
-                
                 if(mat[i][j] == 0){
-                    ans[i][j] = 0;
+                    dist[i][j] = 0;
                     q.add(new Node(i,j,0));
                     vis[i][j] = 1;
                 }
@@ -34,30 +30,31 @@ class Solution {
                 }
             }
         }
-        
-        int[] delrow = {-1,0,1,0};
-        int[] delcol = {0,1,0,-1};
+    
+        int[] delRow = {-1,0,1,0};
+        int[] delCol = {0,1,0,-1};
         
         while(!q.isEmpty()){
-            
-            int row = q.peek().row;
-            int col = q.peek().col;
+            int row = q.peek().first;
+            int col = q.peek().second;
             int steps = q.peek().dist;
-            ans[row][col] = steps;
             
+            dist[row][col] = steps;
             q.remove();
-            
-            for(int i=0;i<4;i++){
-                int nrow = row + delrow[i];
-                int ncol = col + delcol[i];
+            for(int i=0 ; i<4; i++){
                 
-                if(nrow>=0 && nrow<n && ncol>=0  && ncol<m && vis[nrow][ncol] == 0){
-                            vis[nrow][ncol] = 1; 
+                int nrow = row + delRow[i];
+                int ncol = col + delCol[i];
+                
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol] == 0){
+                    
+                    vis[nrow][ncol] = 1;
                     q.add(new Node(nrow,ncol,steps+1));
                     
                 }
-            }        
+                
+            }
         }
-        return ans;
+        return dist;
     }
 }
