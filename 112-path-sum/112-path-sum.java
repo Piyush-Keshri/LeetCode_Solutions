3e62f0906496c9174de0fd12a41ced159b179430
Piyush Keshri <69593831+Piyush-Keshri@users.noreply.github.com>
2022-10-04 11:19:40 +0530

    Time: 1 ms (69.13%), Space: 44.2 MB (12.41%) - LeetHub

--- 112-path-sum/112-path-sum.java
+++ 112-path-sum/112-path-sum.java
@@ -1,24 +1,12 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
 class Solution {
-    public boolean hasPathSum(TreeNode root, int sum) {
-        if(root == null)
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+            
+        if(root == null){
             return false;
+        }
         else
-            return (root.left == null && root.right == null && root.val == sum)
-            || hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+        return(root.left == null && root.right == null && root.val == targetSum) || hasPathSum(root.left,targetSum-root.val) || hasPathSum(root.right,targetSum-root.val);
+        
     }
 }
