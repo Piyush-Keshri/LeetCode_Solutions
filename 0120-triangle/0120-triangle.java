eede3767b8098d18456445c73a145a078b8bbefb
Piyush Keshri <69593831+Piyush-Keshri@users.noreply.github.com>
2023-01-09 23:28:16 +0530

    Time: 1 ms (100.00%), Space: 42.1 MB (71.82%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -2,18 +2,18 @@ class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         
         int n = triangle.size();
-        int[][] dp = new int[n][n]; 
         
-        for(int[] row: dp){
-            Arrays.fill(row,Integer.MAX_VALUE);
+        int[][] dp = new int[n][n];
+        
+        for(int[] arr : dp){
+            Arrays.fill(arr,Integer.MAX_VALUE);            
         }
         
         return memoization(triangle,0,0,dp);
     }
     
     public int memoization(List<List<Integer>> triangle,int row,int col,int[][] dp){
-       
-//         Base Case
+        
         if(row == triangle.size()){
             return 0;
         }
@@ -22,13 +22,11 @@ class Solution {
             return dp[row][col];
         }
         
-//         We can traverse either down or diagonal
+        int traverse = Math.min(memoization(triangle,row+1,col,dp),memoization(triangle,row+1,col+1,dp));
         
-int traverse = Math.min(memoization(triangle,row+1,col,dp),memoization(triangle,row+1,col+1,dp));
-
-        dp[row][col] = triangle.get(row).get(col) + traverse;
+        dp[row][col] = triangle.get(row).get(col) + traverse; 
         
-        return dp[row][col];        
+        return dp[row][col];
     }
     
 }
