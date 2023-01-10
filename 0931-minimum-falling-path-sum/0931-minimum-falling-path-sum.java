1399afc81670bf1dc8ad19555705e79ffcf8780d
Piyush Keshri <69593831+Piyush-Keshri@users.noreply.github.com>
2023-01-11 00:00:33 +0530

    Time: 1 ms (99.81%), Space: 43 MB (73.31%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,38 +1,59 @@
 class Solution {
-    
     public int minFallingPathSum(int[][] matrix) {
+        
         int n = matrix.length;
-        int min = Integer.MAX_VALUE;
-
-        for(int i=1;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                int left = matrix[i][j];
-                int right = matrix[i][j];
-                int mid = matrix[i][j]+matrix[i-1][j];
-                if(j>0&&j<n-1)
-                {
-                    left += matrix[i-1][j-1];
-                    right += matrix[i-1][j+1];
-                    matrix[i][j] = Math.min(left,Math.min(right,mid));
-                }
-                else if(j>0)
-                {
-                    left += matrix[i-1][j-1];
-                    matrix[i][j] = Math.min(left,mid);
-                }
-                else if(j<n-1)
-                {
-                    right += matrix[i-1][j+1];
-                    matrix[i][j] = Math.min(right,mid);
-                }
-                if(i==n-1)
-                {
-                    min = Math.min(min,matrix[i][j]);
-                }
-            }
+        
+        int[][] dp = new int[n][n];
+        
+        for(int[] row: dp){
+            Arrays.fill(row,(int)1e4);
         }
-        return min==Integer.MAX_VALUE?matrix[0][0]:min;
+        
+        int mini = Integer.MAX_VALUE;
+        
+        for(int col = 0; col<n;col++){
+            int ans = memoization(n-1,col,n,matrix,dp);
+            mini = Math.min(mini,ans);
+        }
+        return mini;
     }
+    
+    public int memoization(int row,int col,int n,int[][] matrix,int[][] dp){
+        
+        if(col<0 || col>=n){
+            return (int)1e4;
+        }
+        
+        if(row == 0){
+            return matrix[0][col];
+        }
+        
+        if(dp[row][col] != (int)1e4){
+            return dp[row][col];
+        }
+        
+        int up = matrix[row][col] + memoization(row-1,col,n,matrix,dp);
+        int ld = matrix[row][col] + memoization(row-1,col-1,n,matrix,dp);
+        int ud = matrix[row][col] + memoization(row-1,col+1,n,matrix,dp);
+        
+        return dp[row][col] = Math.min(up,Math.min(ld,ud));
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
