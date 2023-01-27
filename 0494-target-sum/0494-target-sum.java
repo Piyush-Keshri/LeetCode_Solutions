1361fa68bf0d9f7df5a0a32d30a56eef6341ad9c
Piyush Keshri <69593831+Piyush-Keshri@users.noreply.github.com>
2023-01-27 18:16:00 +0530

    Time: 4 ms (95.35%), Space: 41.7 MB (63.43%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,67 +1,39 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-     
-    int n = nums.length;
-    int totSum = 0;
-    for(int x:nums){
-        totSum +=x; 
-    }
-    
-    if(totSum-target < 0) return 0;
-    if((totSum-target)%2 == 1) return 0;    
+        int n = nums.length;
         
-    int s2 = (totSum-target)/2;
+        int totSum = 0;
         
+        for(int x : nums){
+            totSum += x;
+        }
         
-    int[][] dp = new int[n][s2+1];
-    
-    for(int[] rows: dp){
-        Arrays.fill(rows,-1);
-    }    
-        return memoization(n-1,s2,nums,dp);
+        if(totSum < target || (totSum-target)%2 != 0) return 0;
         
-    }
-    
-    public int memoization(int indx,int target,int[] nums,int[][] dp){
+        int s = (totSum-target)/2;
+        
+        int[][] dp = new int[n][s+1];
+        
+        if(nums[0] == 0) dp[0][0] = 2;
+        else dp[0][0] = 1;
+        
+        if(nums[0]!=0 && nums[0]<=s) dp[0][nums[0]] = 1;
+        
+        for(int indx = 1;indx<n;indx++){
             
-        if(indx == 0){
+            for(int tar = 0;tar<=s;tar++){
+                
+                int notTake = dp[indx-1][tar];
+                int take = 0;
+                
+                if(nums[indx]<=tar){
+                    take = dp[indx-1][tar-nums[indx]];
+                }
+                
+                dp[indx][tar]  = take+notTake;
+            }
             
-            if(target == 0 && nums[0] == 0){
-                return 2;
-            }
-            if(target == 0 || nums[0] == target){
-                return 1;
-            }
-            return 0;
         }
-        
-        if(dp[indx][target] != -1){
-            return dp[indx][target];
-        }
-        
-        int notTake = memoization(indx-1,target,nums,dp);
-        int take = 0;
-        
-        if(nums[indx]<= target){
-            take = memoization(indx-1,target-nums[indx],nums,dp); 
-        }
-        return dp[indx][target] = take+notTake;
+        return dp[n-1][s];
     }
-    
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
