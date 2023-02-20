c9f3cf743203c9c7cfb3e91315a1c83a09d8c03a
Piyush Keshri <69593831+Piyush-Keshri@users.noreply.github.com>
2023-02-20 20:48:55 +0530

    Added solution - LeetHub

--- Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.java
+++ Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.java
@@ -12,31 +12,7 @@ class Solution
 {
     public static void sort012(int a[], int n)
     {
-     
-     int cnt0=0,cnt1=0,cnt2=0;
-     
-     for(int i=0;i<n;i++){
-         if(a[i] == 0){
-             cnt0++;
-         }
-         if(a[i] == 1){
-             cnt1++;
-         }
-         if(a[i] == 2){
-             cnt2++;
-         }
-     }
-     
-     for(int i =0;i<n;i++){
-         a[i] = 0;
-     }
-     for(int i =cnt0;i<n;i++){
-         a[i] = 1;
-     }
-     for(int i =cnt0+cnt1;i<n;i++){
-         a[i] = 2;
-     }
-     
+       Arrays.sort(a);
     }
 }
 
