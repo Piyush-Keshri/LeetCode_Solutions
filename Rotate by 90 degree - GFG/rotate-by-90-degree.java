82b94c13d497fa6bda06b9b079c972570b58d0a7
Piyush Keshri <69593831+Piyush-Keshri@users.noreply.github.com>
2023-03-09 23:47:21 +0530

    Added solution - LeetHub

---
+++ Rotate by 90 degree - GFG/rotate-by-90-degree.java
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t= Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            int n = Integer.parseInt(read.readLine());
+            int matrix[][] = new int[n][n];
+            String st[] = read.readLine().trim().split("\\s+");
+            int k = 0;
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                    matrix[i][j] = Integer.parseInt(st[k++]);
+            }
+            Solution ob = new Solution();
+            ob.rotateby90(matrix, n);
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                    System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to rotate matrix anticlockwise by 90 degrees.
+    static void rotateby90(int matrix[][], int n) 
+    { 
+        int[][] mat = new int[n][n];
+        
+        for(int i = 0;i<n;i++){
+            for(int j = n-1;j>=0;j--){
+                
+                mat[n-1-j][i] = matrix[i][j];
+                
+            }
+        }
+        
+        for(int i = 0;i<n;i++){
+            for(int j = n-1;j>=0;j--){
+                
+                matrix[i][j] = mat[i][j];
+                
+            }
+        }
+        
+    }
+}
