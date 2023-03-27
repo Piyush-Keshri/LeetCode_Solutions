273a56eef551dff0f81bf477e807619cf8b70bb1
Piyush Keshri <69593831+Piyush-Keshri@users.noreply.github.com>
2023-03-27 23:51:45 +0530

    Added solution - LeetHub

---
+++ Smallest Divisor - GFG/smallest-divisor.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t;
+        t = sc.nextInt();
+        while (t-- > 0) {
+
+            int n = sc.nextInt();
+            int K;
+            K = sc.nextInt();
+            int[] nums = new int[n];
+            for (int i = 0; i < n; i++) nums[i] = sc.nextInt();
+
+            Solution obj = new Solution();
+            int res = obj.smallestDivisor(nums, K);
+
+            System.out.println(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Return the sum of division results with 'divisor'.
+    private int findDivisionSum(int[] nums, int divisor) {
+        int result = 0;
+        for (int num : nums) {
+            result += Math.ceil((1.0 * num) / divisor);
+        }
+        return result;
+    }
+    
+    public int smallestDivisor(int[] nums, int threshold) {
+        int ans = -1;
+        
+        int low = 1;
+        int high = nums[0];
+        for (int num : nums) {
+            high = Math.max(high, num);
+        }
+        
+        // Iterate using binary search on all divisors.
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int result = findDivisionSum(nums, mid);
+            // If current divisor does not exceed threshold, 
+            // then it can be our answer, but also try smaller divisors
+            // thus change search space to left half.
+            if (result <= threshold) {
+                ans = mid;
+                high = mid - 1;
+            }
+            // Otherwise, we need a bigger divisor to reduce the result sum
+            // thus change search space to right half.
+            else {
+                low = mid + 1;
+            }
+        }
+        
+        return ans;
+    }
+}
