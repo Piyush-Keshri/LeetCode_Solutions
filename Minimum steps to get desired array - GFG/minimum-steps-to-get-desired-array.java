dbc29ffa017f103fa9c64c855fabcc16311b499c
Piyush Keshri <69593831+Piyush-Keshri@users.noreply.github.com>
2023-04-01 11:14:23 +0530

    Added solution - LeetHub

--- Minimum steps to get desired array - GFG/minimum-steps-to-get-desired-array.java
+++ Minimum steps to get desired array - GFG/minimum-steps-to-get-desired-array.java
@@ -31,38 +31,46 @@ public class Main {
 
 class Solution {
     int countMinOperations(int[] arr, int n) {
-        int result = 0;
-      
-        while (true)
-        {
-            int zero_count = 0;
-      
+        
+        int ans= 0;
+        
+        while(true){
+            
+            int zeros = 0;
             int i;
-            for (i=0; i<n; i++)
-            {
-                if (arr[i] % 2 == 1)
+            
+            for(i = 0;i<n;i++){
+                
+                if(arr[i]%2 == 1){
                     break;
-                else if (arr[i] == 0)
-                    zero_count++;
+                }
+                else if(arr[i] == 0){
+                    zeros++;
+                }
             }
-
-            if (zero_count == n)
-              return result;
-            if (i == n)
-            {
-                for (int j=0; j<n; j++)
-                   arr[j] = arr[j]/2;
-                result++;
+            
+            if(zeros == n){
+                return ans;
             }
-            for (int j=i; j<n; j++)
-            {
-               if (arr[j] %2 == 1)
-               {
-                  arr[j]--;
-                  result++;
-               }
+            
+            if(i == n){
+                for(int j = 0;j<n;j++){
+                    arr[j]/=2;
+                }
+                ans++;
             }
+            
+            for(int j = 0;j<n;j++){
+                
+                if(arr[j]%2 == 1){
+                    
+                    arr[j]--;
+                    ans++;
+                }
+                
+            }
+            
         }
+        
     }
-    
 }
