4aa58f6dca7f89e470438d2e26ec9546a96e5255
Piyush Keshri <69593831+Piyush-Keshri@users.noreply.github.com>
2023-04-02 12:29:14 +0530

    Time: 7 ms (100.00%), Space: 43.5 MB (100.00%) - LeetHub

--- 2606-find-the-substring-with-maximum-cost/2606-find-the-substring-with-maximum-cost.java
+++ 2606-find-the-substring-with-maximum-cost/2606-find-the-substring-with-maximum-cost.java
@@ -1,16 +1,19 @@
 class Solution {
     public int maximumCostSubstring(String s, String chars, int[] vals) {
-        
+    
+//         Enter the value of each alphabet from 1 to 26.
        int[] arr = new int[26];
         for(int i=0;i<26;i++){
             arr[i] = i+1;
         }
         
+//      Update the value of alphabets as per the question from vals array.   
         for(int i = 0;i<chars.length();i++){
             int curr = chars.charAt(i)-'a';
             arr[curr] = vals[i]; 
         }
         
+//      Create a array of values of the given 's' String.  
         int[] subarr = new int[s.length()];
         
         for(int i =0;i<s.length();i++){
@@ -18,7 +21,7 @@ class Solution {
             subarr[i] = arr[curr];
         }
         
-        //Applying Kadanes Algorithm to find maximum subarray sum
+//      Applying Kadanes Algorithm to find maximum subarray sum.
         
         int total = 0;
        int maxi = subarr[0];
@@ -33,6 +36,8 @@ class Solution {
                 total = 0;
             }
         }
+        
+//      if maxi <0 return 0 .   
         return maxi<0?0:maxi;
     }
 }
