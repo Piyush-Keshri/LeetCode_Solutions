0055c19069430cef4389d7974adbc19d271d91b5
Piyush Keshri <69593831+Piyush-Keshri@users.noreply.github.com>
2023-04-06 11:50:02 +0530

    Time: 4 ms (17.19%), Space: 43 MB (51.95%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.java
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -2,12 +2,13 @@ class Solution {
     public int closedIsland(int[][] grid) {
         
         int m = grid.length;
-        int n = grid[0].length;
-        boolean[][] vis = new boolean[m][n]; 
+        int n =  grid[0].length;
+        
+        boolean[][] vis = new boolean[m][n];
+        
         int island = 0;
         
-        for(int i = 0;i<m;i++){
-            
+        for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 
                 if(grid[i][j] == 0 && !vis[i][j] && bfs(i,j,m,n,grid,vis)){
@@ -15,16 +16,15 @@ class Solution {
                 }
                 
             }
-            
         }
         return island;
     }
     
-    public boolean bfs(int row,int col,int m,int n,int[][] grid,boolean[][] vis){
+    public boolean bfs(int row,int col,int m,int n,int[][] grid,boolean[][]vis){
         
         Queue<int[]> q = new LinkedList<>();
         q.offer(new int[]{row,col});
-        vis[row][col] = true;
+        
         boolean isClosed = true;
         
         int[] delRow = {0,1,0,-1};
@@ -38,12 +38,14 @@ class Solution {
             
             for(int i=0;i<4;i++){
                 
-                int nrow = row+delRow[i];
-                int ncol = col+delCol[i];
+                int nrow = row + delRow[i];
+                int ncol = col + delCol[i];
                 
                 if(nrow<0 || ncol<0 || nrow>m-1 || ncol>n-1){
+                    
                     isClosed = false;
                 }
+                
                 else if(grid[nrow][ncol] == 0 && !vis[nrow][ncol]){
                     q.offer(new int[]{nrow,ncol});
                     vis[nrow][ncol] = true;
@@ -54,9 +56,7 @@ class Solution {
         }
         return isClosed;
     }
-    
 }
-
 
 
 
