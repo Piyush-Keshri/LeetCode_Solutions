794cdcd9314c7029650ee29d3c082c6a2106d8f2
Piyush Keshri <69593831+Piyush-Keshri@users.noreply.github.com>
2023-05-23 18:21:22 +0530

    Time: 1 ms (98.47%), Space: 44.4 MB (6.05%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> ls = new ArrayList<>();
+        boolean[] vis = new boolean[nums.length];
+        
+        solve(nums,vis,ls,res);
+        
+        return res;
+    }
+    
+    public void solve(int[] nums,boolean[]vis,List<Integer> ls,List<List<Integer>>res){
+        
+        if(ls.size() == nums.length){
+            res.add(new ArrayList<>(ls));
+            return;
+        }
+        
+        for(int i=0;i<nums.length;i++){
+            
+            if(vis[i] == false){
+                vis[i] = true;
+                ls.add(nums[i]);
+                solve(nums,vis,ls,res);
+                ls.remove(ls.size()-1);
+                vis[i] = false;
+            }
+            
+        }
+        
+    } 
+    
+}
