c8e3f962cf494ebd3baf544470b233a19fb4a585
Piyush Keshri <69593831+Piyush-Keshri@users.noreply.github.com>
2023-05-27 17:04:26 +0530

    Added solution - LeetHub

--- Number of Unique Paths - GFG/number-of-unique-paths.java
+++ Number of Unique Paths - GFG/number-of-unique-paths.java
@@ -35,35 +35,36 @@ class Solution
     //Function to find total number of unique paths.
     public static int NumberOfPath(int a, int b) 
     {
+       
        int[][] dp = new int[a+1][b+1];
        
-       for(int[] row:dp){
+       for(int[] row : dp){
+           
            Arrays.fill(row,-1);
        }
        
-       return memo(a-1,b-1,dp);
+       for(int i=0;i<a;i++){
+           
+           for(int j=0;j<b;j++){
+               
+               if(i ==0 && j==0){
+                   dp[0][0] = 1;
+                   continue;
+               }
+        int up=0;
+          int left = 0;
+               if(i>0){
+                   up = dp[i-1][j];
+               }
+               if(j>0){
+                   left = dp[i][j-1];
+               }
+               dp[i][j] = up+left;
+               
+           }
+           
+       }
        
+       return dp[a-1][b-1];
     }
-    
-    public static int memo(int row,int col,int[][] dp){
-        
-        if(row == 0 && col == 0){
-            return  1;
-        }
-        
-        if(row<0 || col<0){
-            return  0;
-        }
-        
-        if(dp[row][col]!= -1){
-            return dp[row][col];
-        }
-        
-        int up = memo(row-1,col,dp);
-        int left = memo(row,col-1,dp);
-        
-        return dp[row][col] = up+left;
-        
-    }
-    
 }
