3b9ea4ac7aa7d92cf71b13cd66909f53c37e8320
Piyush Keshri <69593831+Piyush-Keshri@users.noreply.github.com>
2023-05-27 17:13:53 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (75.35%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,41 +1,43 @@
-public class Solution {
-public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-    int m = obstacleGrid.length;
-    int n = obstacleGrid[0].length;
-    int[][] s = new int[m][n];
-    s[0][0] = obstacleGrid[0][0]==0 ? 1:0;
-    if(s[0][0] == 0) return 0;
-    for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            if(obstacleGrid[i][j] == 1) s[i][j] = 0;
-            else if(i==0){
-                if(j>0) s[i][j] = s[i][j-1];
-            }
-            else if(j==0){
-                if(i>0) s[i][j] = s[i-1][j];
-            }
-            else s[i][j] = s[i-1][j] + s[i][j-1];
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+       int n = obstacleGrid.length;
+       int m = obstacleGrid[0].length; 
+       
+        if(obstacleGrid[n-1][m-1] == 1 ||obstacleGrid[0][0] == 1  ){
+            return 0;
         }
+        
+       int[][] dp = new int[n+1][m+1];
+        
+        for(int[] row : dp){
+            Arrays.fill(row,-1);
+        }
+        
+      return memo(n-1,m-1,obstacleGrid,dp);  
     }
-    return s[m-1][n-1];
+    
+    public int memo(int row,int col,int[][] grid,int[][] dp){
+        
+        if(row == 0 && col == 0){
+            return 1;
+        }
+        if(row<0 || col<0){
+            return 0;
+        }
+        
+        if(dp[row][col]!=-1){
+            return dp[row][col];
+        }
+        
+        int up=0;int left = 0;
+        
+        if(grid[row][col] != 1){
+            
+            up = memo(row-1,col,grid,dp);
+            left = memo(row,col-1,grid,dp);
+            
+        }
+        return dp[row][col] = up+left;
+    }
+    
 }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
