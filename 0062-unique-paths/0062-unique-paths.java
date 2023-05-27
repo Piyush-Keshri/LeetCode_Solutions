d1f14486be39f24b2f506d68ba8ddb9b93df3efa
Piyush Keshri <69593831+Piyush-Keshri@users.noreply.github.com>
2023-05-27 16:31:03 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (42.59%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,34 +1,30 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-        
-        for(int[] row : dp){
-            Arrays.fill(row,-1);
+        int[][] dp = new int[m+1][n+1];
+        for(int[] row:dp){
+           Arrays.fill(row,-1); 
         }
-        
-       return tabulation(m,n,dp);
-        
+        return memo(m-1,n-1,dp);
     }
     
-    public int tabulation(int row,int col,int[][]dp){
+    public int memo(int row,int col,int[][] dp){
         
-        for(int i = 0; i<row;i++){
-            dp[i][0] = 1;
+        if(row == 0 || col == 0){
+            return dp[row][col] = 1;
         }
         
-        for(int j= 0;j<col;j++){
-            dp[0][j]=1;
+        if(row<0 || col<0){
+            return dp[row][col] = 0;
         }
         
-        for(int i = 1;i<row;i++){
-            
-            for(int j = 1;j<col;j++){
-                
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
-                
-            }
-            
+        if(dp[row][col] != -1){
+            return dp[row][col];
         }
-        return dp[row-1][col-1];
+        
+        int up = memo(row-1,col,dp);
+        int left = memo(row,col-1,dp);
+        
+        return dp[row][col] = up+left;
     }
+    
 }
