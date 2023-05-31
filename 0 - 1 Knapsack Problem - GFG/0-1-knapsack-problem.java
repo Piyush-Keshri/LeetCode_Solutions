e5c54e6fdabc538cf09e2ee548688d7095412de6
Piyush Keshri <69593831+Piyush-Keshri@users.noreply.github.com>
2023-05-31 16:54:15 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -48,30 +48,44 @@ class gfg
 
 class Solution 
 { 
+    //Function to return max value that can be put in knapsack of capacity W.
     static int knapSack(int W, int wt[], int val[], int n) 
     { 
-        int dp[][] = new int[n][W+1];
         
-        for(int i = wt[0];i<=W;i++){
-            dp[0][i] = val[0];
+        int[][] dp = new int[n+1][W+1];
+        
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
         }
         
-        for(int i = 1;i<n;i++){
-            
-            for(int j = 0 ;j<=W;j++){
-                int notTake = dp[i-1][j];
-                
-                int take = Integer.MIN_VALUE;
-                
-                if(wt[i] <= j){
-                    take = val[i] + dp[i-1][j-wt[i]];
-                }
-                dp[i][j] = Math.max(take,notTake);
-            }
-            
-        }
-        return dp[n-1][W];
+        return solve(n-1,W,wt,val,dp);
+        
     } 
+    
+    static int solve(int indx,int W,int[] wt,int[] val,int[][] dp){
+        
+        if(indx == 0){
+            
+            if(W>=wt[0]) return val[0];
+            return 0;
+            
+        }
+        
+        if(dp[indx][W] != -1){
+            return dp[indx][W];
+        }
+        
+        int notTake = 0 + solve(indx-1,W,wt,val,dp);
+        int take = 0;
+        
+        if(W>=wt[indx]){
+            
+            take = val[indx] + solve(indx-1,W-wt[indx],wt,val,dp);
+            
+        }
+        return dp[indx][W] = Math.max(take,notTake);
+    }
+    
 }
 
 
