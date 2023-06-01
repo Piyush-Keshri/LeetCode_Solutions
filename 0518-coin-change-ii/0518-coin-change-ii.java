837e987bdb05cb2cb386cdf9abefec56b9423ba0
Piyush Keshri <69593831+Piyush-Keshri@users.noreply.github.com>
2023-06-01 12:59:40 +0530

    Time: 7 ms (48.96%), Space: 45.8 MB (56.39%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -3,33 +3,25 @@ class Solution {
         int n = coins.length;
         int[][] dp = new int[n+1][amount+1];
         
-        for(int[] row:dp)
-        {Arrays.fill(row,-1);}
-    
-        return solve(n-1,amount,coins,dp);
-        
-    }
-    
-    public int solve(int indx,int amount,int[] coins,int[][] dp){
-        
-        if(indx == 0){
-            if(amount%coins[0] == 0) return 1;
-           
-            return 0;
+        for(int amnt = 0;amnt<=amount;amnt++){
+            if(amnt % coins[0] == 0) dp[0][amnt] = 1;
         }
         
-        if(dp[indx][amount] != -1){
-            return dp[indx][amount];
+        
+        for(int indx = 1;indx<n;indx++){
+            
+            for(int amnt =0;amnt<=amount;amnt++){
+                
+                int nt = dp[indx-1][amnt];
+                int tk = 0;
+                
+                if(amnt>=coins[indx]){
+                    tk = dp[indx][amnt-coins[indx]];
+                }
+                dp[indx][amnt] = tk+nt;
+            }            
         }
-        
-        int nt = solve(indx-1,amount,coins,dp);
-        int tk = 0;
-        
-        if(amount >= coins[indx]){
-            tk = solve(indx,amount-coins[indx],coins,dp);
-        }
-        
-        return dp[indx][amount] = tk + nt;
-        
+            
+        return dp[n-1][amount];
     }
 }
