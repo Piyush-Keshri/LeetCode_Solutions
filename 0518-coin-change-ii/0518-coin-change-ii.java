025f99bb357b7ea90a65b1602b0b81be04eab851
Piyush Keshri <69593831+Piyush-Keshri@users.noreply.github.com>
2023-06-01 12:42:24 +0530

    Time: 6 ms (62.54%), Space: 45.8 MB (55.87%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,50 +1,35 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        
         int n = coins.length;
-        int[][] dp = new int[n][amount+1];
+        int[][] dp = new int[n+1][amount+1];
         
-        for(int[] row : dp){
-            Arrays.fill(row,-1);
-        }
+        for(int[] row:dp)
+        {Arrays.fill(row,-1);}
+    
+        return solve(n-1,amount,coins,dp);
         
-        return memoization(n-1,amount,coins,dp);
     }
     
-    public int memoization(int indx,int target,int[] coins,int[][] dp){
+    public int solve(int indx,int amount,int[] coins,int[][] dp){
         
         if(indx == 0){
-            
-            if(target%coins[indx] == 0){
-               return 1;
-            }
+            if(amount%coins[0] == 0) return 1;
+           
             return 0;
         }
         
-        if(dp[indx][target] != -1){
-            return dp[indx][target];
+        if(dp[indx][amount] != -1){
+            return dp[indx][amount];
         }
         
-        int notTake = memoization(indx-1,target,coins,dp);
+        int nt = solve(indx-1,amount,coins,dp);
+        int tk = 0;
         
-        int take = 0;
-        
-        if(coins[indx]<=target){
-            take = memoization(indx,target-coins[indx],coins,dp);
+        if(amount >= coins[indx]){
+            tk = solve(indx,amount-coins[indx],coins,dp);
         }
         
-        return dp[indx][target] = take+notTake;
+        return dp[indx][amount] = tk + nt;
+        
     }
-    
 }
-
-
-
-
-
-
-
-
-
-
-
