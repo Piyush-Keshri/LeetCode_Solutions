66aaa38f14a7a90b00be6a93177edcff183303ab
Piyush Keshri <69593831+Piyush-Keshri@users.noreply.github.com>
2023-06-04 13:45:06 +0530

    Time: 74 ms (26.91%), Space: 55.3 MB (5.63%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,18 +1,39 @@
 class Solution {
     public int longestPalindromeSubseq(String s) {
-        int[][] dp = new int[s.length()][s.length()];
-
-        for (int i = s.length() - 1; i >= 0; i--) {
-            dp[i][i] = 1;
-            for (int j = i + 1; j < s.length(); j++) {
-                if (s.charAt(i) == s.charAt(j)) {
-                    dp[i][j] = dp[i + 1][j - 1] + 2;
-                } else {
-                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
-                }
-            }
+       
+        int n = s.length();
+        
+        StringBuilder sb = new StringBuilder(s);
+        String s1 = sb.reverse().toString();
+        
+        int[][] dp = new int[n+1][n+1];
+        
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
         }
-
-        return dp[0][s.length() - 1];
+        
+        return solve(n-1,n-1,s,s1,dp);
+        
     }
+    
+    public int solve(int indx1,int indx2,String s1,String s2,int[][] dp){
+        
+        if(indx1 < 0 || indx2 < 0){
+            return 0;
+        }
+        
+        if(dp[indx1][indx2] != -1){
+            return dp[indx1][indx2];
+        }
+        
+        if(s1.charAt(indx1) == s2.charAt(indx2)){
+            
+          return  dp[indx1][indx2] = 1+solve(indx1-1,indx2-1,s1,s2,dp);
+            
+        }
+        
+        return dp[indx1][indx2] = Math.max(solve(indx1,indx2-1,s1,s2,dp),solve(indx1-1,indx2,s1,s2,dp));
+        
+    }
+    
 }
