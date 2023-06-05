3c48dda99f3f22ae43765fa6207ebe84e8c46241
Piyush Keshri <69593831+Piyush-Keshri@users.noreply.github.com>
2023-06-05 13:24:02 +0530

    Time: 21 ms (58.55%), Space: 46.8 MB (83.51%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,48 +1,31 @@
 class Solution {
     public int numDistinct(String s, String t) {
+     
         int n = s.length();
         int m = t.length();
         
-        int[][] dp = new int[n][m];
+        int[][] dp = new int[n+1][m+1];
         
-        for(int[] rows:dp){
-            Arrays.fill(rows,-1);
+        for(int i=0;i<n;i++){
+            dp[i][0] = 1;
         }
         
-       return solve(n-1,m-1,s,t,dp);
-        
+        for(int i=1;i<=n;i++){
+            
+            for(int j=1;j<=m;j++){
+                
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                    
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+                
+            }
+            
+        }
+      return dp[n][m];  
     }
-    
-    public int solve(int indx1,int indx2,String s1,String s2,int[][] dp){
-        if(indx2 < 0){
-            return 1;
-        }
-        
-        if(indx1 < 0){
-            return 0; 
-        }
-        
-        
-        if(dp[indx1][indx2] != -1){
-            return dp[indx1][indx2];
-        }
-        
-        if(s1.charAt(indx1) == s2.charAt(indx2)){
-           return  dp[indx1][indx2] =( solve(indx1-1,indx2-1,s1,s2,dp) + solve(indx1-1,indx2,s1,s2,dp));
-        }    
-        else
-       return dp[indx1][indx2] = solve(indx1-1,indx2,s1,s2,dp);
-        
-    }
-
 }
-
-
-
-
-
-
-
-
-
-
