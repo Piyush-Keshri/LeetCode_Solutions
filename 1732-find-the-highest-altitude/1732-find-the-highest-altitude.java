8831d2ffcdf44d508e0b4f12ac22165f38c21f1f
Piyush Keshri <69593831+Piyush-Keshri@users.noreply.github.com>
2023-06-19 11:26:05 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (73.24%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -5,18 +5,14 @@ class Solution {
         int[] alt = new int[n+1];
         
         alt[0] = 0;
+        int max = 0;
         
         for(int i=1;i<n+1;i++){
             alt[i] = alt[i-1]+gain[i-1];
+            
+            max = Math.max(max,alt[i]);
         }
         
-        int max = Integer.MIN_VALUE;
-        
-        for(int x:alt){
-            
-            max = Math.max(max,x);
-            
-        }
         return max;
     }
 }
