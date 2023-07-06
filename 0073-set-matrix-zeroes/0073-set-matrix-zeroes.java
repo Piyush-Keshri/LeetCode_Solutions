a53e38632dcdd2a0a60569ab7639afbfb69855f8
Piyush Keshri <69593831+Piyush-Keshri@users.noreply.github.com>
2023-07-06 14:21:37 +0530

    Time: 0 ms (100.00%), Space: 44.6 MB (71.36%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,38 +1,30 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
         
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        int[][] matrix2 = new int[rows][cols];
+        int[] row = new int[m];
+        int[] col = new int[n];
         
-         for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++)
-                matrix2[i][j]=matrix[i][j];
-        }
-        
-        for(int i = 0;i<rows;i++){
-            for(int j = 0;j<cols;j++){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 
                 if(matrix[i][j] == 0){
-                    
-                    for(int k =0;k<rows;k++){
-                        matrix2[k][j] = 0;
-                    }
-                    for(int k =0;k<cols;k++){
-                        matrix2[i][k] = 0;
-                    }
+                    row[i] = 1;
+                    col[j] = 1;
                 }
                 
             }
         }
-        
-        for(int i = 0;i<rows;i++){
-            for(int j = 0;j<cols;j++){
+         
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 
-                matrix[i][j] = matrix2[i][j];
+                if(row[i] == 1 || col[j] == 1){
+                    matrix[i][j] = 0;
+                }
                 
             }
         }
-        
     }
 }
