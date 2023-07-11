c553651f2083fe8cb06ddbe8edc604979f5e3701
Piyush Keshri <69593831+Piyush-Keshri@users.noreply.github.com>
2023-07-11 17:03:58 +0530

    Time: 13 ms (34.09%), Space: 46.9 MB (44.22%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,70 +1,29 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        
-        int num1 =Integer.MIN_VALUE,num2 = Integer.MIN_VALUE,cnt1 =0,cnt2 = 0;
-        
-        for(int i = 0;i<nums.length;i++){
-            
-            if(num1 == nums[i]){
-                cnt1++;
-            }
-            else if(num2 == nums[i]){
-                cnt2++;
-            }
-            else if(cnt1 == 0){
-                num1 = nums[i];
-                cnt1 = 1;
-            }
-            else if(cnt2 == 0){
-                num2 = nums[i];
-                cnt2 = 1;
-            }
-            else{
-                cnt1--;
-                cnt2--;
-            }
-        }
-        
-        cnt1 = 0;cnt2 = 0;
-        
-        List<Integer> ans = new ArrayList<>();
-        
+         Map<Integer,Integer> map = new HashMap<>();
+        List<Integer> ls = new ArrayList<>();
+        int n = nums.length;
         for(int i=0;i<nums.length;i++){
             
-            if(nums[i] == num1){
-                cnt1++;
+            if(!map.containsKey(nums[i])){
+                map.put(nums[i],1);
             }
-            if(nums[i] == num2){
-                cnt2++;
+            else{
+                map.put(nums[i],map.get(nums[i])+1);
             }
+            
         }
         
-        if(cnt1>(nums.length/3)){
-            ans.add(num1);
+        for(Map.Entry<Integer,Integer> it : map.entrySet()){
+            
+            int val = it.getValue();
+            
+            if(val > n/3){
+                ls.add(it.getKey());
+            }
+            
         }
-        if(cnt2>(nums.length/3)){
-            ans.add(num2);
-        }
-        
-        return ans;
-        
+        return ls;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
