8f117dff16996feabd69cedb33f2ccafa16426e8
Piyush Keshri <69593831+Piyush-Keshri@users.noreply.github.com>
2023-07-25 13:42:19 +0530

    Added solution - LeetHub

--- Number of occurrence - GFG/number-of-occurrence.java
+++ Number of occurrence - GFG/number-of-occurrence.java
@@ -37,16 +37,70 @@ public class Main {
 
 class Solution {
     int count(int[] arr, int n, int x) {
-       
-       int cnt = 0;
-       
-       for(int i = 0;i<n;i++){
-           
-           if(arr[i] == x){
-               cnt++;
-           }
-           
-       }
-       return cnt;
+        int last = findLast(arr,n,x);
+        int first = findFirst(arr,n,x);
+        
+        return last-first+1;    
+        
     }
+    
+    
+    int findFirst(int[] arr,int n,int target){
+        
+        int lo = 0;
+        int hi = arr.length-1;
+        int ans = 0;
+        while(lo<=hi){
+            
+            int mid = lo+(hi-lo)/2;
+            
+            if(arr[mid] == target){
+                
+                ans = mid;
+                hi = mid-1;
+                
+            }
+            
+            else if(arr[mid] > target){
+                hi = mid-1;
+            }
+            else{
+                lo = mid+1;
+            }
+            
+        }
+        return ans;
+    }
+    
+    
+    int findLast(int[] arr,int n,int target){
+        
+        int lo = 0;
+        int hi = arr.length-1;
+        int ans = -1;
+        while(lo<=hi){
+            
+            int mid = lo+(hi-lo)/2;
+            
+            if(arr[mid] == target){
+                
+                ans = mid;
+                lo = mid+1;
+                
+            }
+            
+            else if(arr[mid] > target){
+                hi = mid-1;
+            }
+            else{
+                lo = mid+1;
+            }
+            
+        }
+        return ans;
+    }
+    
+    
+    
+    
 }
