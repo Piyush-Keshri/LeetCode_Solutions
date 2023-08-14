405e06dc14472259eef0966ed4832c9b3f2b4456
Piyush Keshri <69593831+Piyush-Keshri@users.noreply.github.com>
2023-08-14 11:41:13 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (91.35%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -1,20 +1,88 @@
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         
-        if(p==null && q==null){
+        if(p == null && q == null){
             return true;
         }
         
         if(p == null || q == null){
             return false;
         }
+    
+        Queue<TreeNode> q1 = new LinkedList<>();
+        Queue<TreeNode> q2 = new LinkedList<>();
         
-        if(p.val != q.val){
-            return false;
-        }
+        q1.add(p);
+        q2.add(q);
         
-        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
-        
+        while(!q1.isEmpty() && !q2.isEmpty()){
+            
+            TreeNode curr1 = q1.poll();
+            TreeNode curr2 = q2.poll();
+            
+            if(curr1.val != curr2.val){
+                return false;
+            }
+            
+            if((curr1.left != null && curr2.left == null) || (curr1.left == null && curr2.left != null)){
+                return false;
+            }
+            
+            if((curr1.right != null && curr2.right == null) || (curr1.right == null && curr2.right != null)){
+                return false;
+            }
+            
+             if (curr1.left != null && curr2.left != null) {
+                q1.add(curr1.left);
+                q2.add(curr2.left);
+            }
+ 
+            if (curr1.right != null
+                && curr2.right != null) {
+                q1.add(curr1.right);
+                q2.add(curr2.right);   
+        }   
     }
+        return true;
 }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
